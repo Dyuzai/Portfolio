c4846b33185fc97edfa44c56e74b6d2fae07a52a
Dyuzai <endrywlucas37@gmail.com>
2025-08-07 16:52:39 -0300

    Criação section page home e correção de código

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
+import Home from './pages/home.jsx'
 
 function App(){
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
--- Portfolio/src/components/header.jsx
+++ Portfolio/src/components/header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <header className="flex justify-between">
-      <h1 className="text-lg font-bold ">Endryw Lucas</h1>
+    <header className="flex w-screen justify-between items-center bg-black fixed p-5 pl-8 pr-8">
+      <h1 className="text-3xl font-bold text-white ">Endryw L.</h1>
       <nav>
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 text-xl ">
           <li><a href="#home">Home</a></li>
           <li><a href="#sobre">Sobre</a></li>
           <li><a href="#projetos">Projetos</a></li>
